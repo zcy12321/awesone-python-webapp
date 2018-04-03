5f6b0aa0a090d79c491192ff3dda5a9762884ae9
ZhaoChenYnag <750570753@qq.com>
2018-04-03 17:21:52 +0800

    add app.py into repository

--- www/app.py
+++ www/app.py
@@ -1,10 +1,15 @@
 import logging; logging.basicConfig(level=logging.INFO)
+#一行中书写多条句必须使用分号分隔每个语句，否则Python无法识别语句之间的间隔
 import asyncio, os, json, time
 from datetime import datetime
+
 from aiohttp import web
 
 def index(request):
-	return web.Rssponse(body=b'<h1>Awesome</h1>')
+	return web.Response(text='<h1>你好！妳好（繁體字）</h1>', content_type='text/html',charset='utf-8')
+#GB2312是中国规定的汉字编码，简体中文的字符集编码，
+#GBK是GB2312的扩展 ,兼容GB2312、显示繁体中文、日文的假名
+#UTF-8是全世界通用的
 
 @asyncio.coroutine
 def init(loop):
