fab7b2ed224bdf1d12ca1355a6c5f6358d542f94
ZhaoChenYnag <750570753@qq.com>
2018-04-16 21:48:09 +0800

    day-05

--- www/app.py
+++ www/app.py
@@ -14,8 +14,63 @@ def index(request):
 #UTF-8是全世界通用的
 
 @asyncio.coroutine
+def logger_factory(app, handler):
+	@asyncio.coroutine
+	def logger(request):
+		#记录日志
+		logging.info('Request: %s %s' % (request.method, request.path))
+		#继续处理请求
+		return (yield from handler(request))
+	return logger
+
+@asyncio.coroutine
+def response_factory(app, handler):
+	@asyncio.coroutine
+	def response(request):
+		#结果
+		r = yield from handler(request)
+		if isinstance(r, web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet=stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if isinstance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp 
+			else:
+				resp = web.Response(body=app['__template__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r, int) and r >= 100 and r < 60:
+			return web.Response(r)
+		if isinstance(r, tuple) and len(r) == 2:
+			t, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.Response(t, str(m))
+		#default
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
+
+
+
+@asyncio.coroutine
 def init(loop):
-	app = web.Application(loop=loop)
+	app = web.Application(loop=loop, middlewares=[logger_factory, response_factory])
+	init_jinja2(app, filters=dict(datetime=datetime_filter))
+	add_routes(app, 'handlers')
+	add_static(app)
 	app.router.add_route('GET', '/', index)
 	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
 	logging.info('server started at http://127.0.0.1:9000...')
