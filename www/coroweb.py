de1ad7d1e3fc4c08c284abe0c0a09aacc72c4581
ZhaoChenYnag <750570753@qq.com>
2018-04-16 21:53:34 +0800

    day-05-coroweb.py

---
+++ www/coroweb.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import functools, asyncio, inspect, logging
+def get(path):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'GET'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+def get(path):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'POST'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+class RequestHandler(object):
+
+	def __init__(self, app, fn):
+		self.app = app
+		self.func = fn
+
+	@asyncio.corouttine
+	def __call__(self, requese):
+		kw = None
+		r = yield from self._func(**kw)
+		return r
+
+def add_route(app, fn):
+	method = getattr(fn,'__method__', None)#存在__method__方法，则返回方法地址，否则返回None
+	path = getattr(fn, '__route__', None)
+	if method is None or path is None:
+		raise ValueError('@get or @post not defined in %s.' % str(fn))
+	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+		fn = asyncio.corouttine(fn)
+	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))
+	app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+	n = module_name.rfind('.')
+	if n == -1:
+		mod = __import__(module_name, globals(), locals())
+	else:
+		name = module_name[n+1:]
+		mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+	for attr in dir(mod):
+		if attr.startswith('_'):
+			continue
+		fn = getattr(mod, attr)
+		if callable(fn):
+			mothod = getattr(fn, '__method__', None)
+			path = getattr(fn, '__route__', None)
+			if method and path:
+				add_route(app, fn)
+				
