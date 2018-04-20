2fae521cc1e478359449ac975ac673a190fd8ebe
ZhaoChenYnag <750570753@qq.com>
2018-04-20 21:40:14 +0800

    day-08 finished

--- www/handlers.py
+++ www/handlers.py
@@ -7,11 +7,16 @@ import asyncio, base64, hashlib, logging, json, time, re
 
 from models import User, Blog, Comment, next_id
 @get('/')
-async def index(request):
-	users = await User.findAll()
+def index(request):
+	summary = 'Lorem ipsum dolor sit amet, consectrtur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+			 Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+			 Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200),
+			 ]
+#	users = await User.findAll()
 	return {
-		'__template__': 'test.html',
-		'users': users
+		'__template__': 'blogs.html',
+		'blogs': blogs
 	}
 #eturn web.Response(text='<h1>你好！妳好（繁體字）</h1>', content_type='text/html',charset='utf-8')
 #GB2312是中国规定的汉字编码，简体中文的字符集编码，
